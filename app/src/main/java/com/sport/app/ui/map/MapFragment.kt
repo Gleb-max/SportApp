219b9e27ebaa4331e78ac8216504c990402f73db
Gleb Demin <gleb.demin.04@mail.ru>
2022-07-15 13:23:47 +0500

    add map

--- app/src/main/java/com/sport/app/ui/map/MapFragment.kt
+++ app/src/main/java/com/sport/app/ui/map/MapFragment.kt
@@ -1,21 +1,56 @@
 package com.sport.app.ui.map
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import com.sport.app.R
+import com.sport.app.core.utils.LocationPermissionHelper
 import com.sport.app.databinding.FragmentMapBinding
+import java.lang.ref.WeakReference
 
 class MapFragment : Fragment() {
 
+    private lateinit var locationPermissionHelper: LocationPermissionHelper
+
     private var _binding: FragmentMapBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        binding.mapView.getMapboxMap().setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+        binding.mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(it).build())
+        binding.mapView.gestures.focalPoint = binding.mapView.getMapboxMap().pixelForCoordinate(it)
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,17 +61,95 @@ class MapFragment : Fragment() {
             ViewModelProvider(this)[MapViewModel::class.java]
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textMap
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        locationPermissionHelper = LocationPermissionHelper(WeakReference(requireActivity()))
+        locationPermissionHelper.checkPermissions {
+            onMapReady()
         }
-        return root
+
+        return binding.root
+    }
+
+    private fun onMapReady() {
+        binding.mapView.apply {
+
+            getMapboxMap().setCamera(
+                CameraOptions.Builder()
+                    .zoom(14.0)
+                    .build()
+            )
+            getMapboxMap().loadStyleUri(
+//                Style.OUTDOORS
+                "mapbox://styles/gleb-max/cl5m6rbl6004l14o82i8q872n"
+            ) {
+                initLocationComponent()
+                setupGesturesListener()
+            }
+        }
+    }
+
+    private fun setupGesturesListener() {
+        binding.mapView.gestures.addOnMoveListener(onMoveListener)
+    }
+
+    private fun initLocationComponent() {
+        val locationComponentPlugin = binding.mapView.location
+        locationComponentPlugin.updateSettings {
+            this.enabled = true
+            this.locationPuck = LocationPuck2D(
+                bearingImage = AppCompatResources.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_launcher_foreground,
+                ),
+//                shadowImage = AppCompatResources.getDrawable(
+//                    requireContext(),
+//                    R.drawable.mapbox_user_icon_shadow,
+//                ),
+                scaleExpression = interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0.0)
+                        literal(0.6)
+                    }
+                    stop {
+                        literal(20.0)
+                        literal(1.0)
+                    }
+                }.toJson()
+            )
+        }
+        locationComponentPlugin.addOnIndicatorPositionChangedListener(
+            onIndicatorPositionChangedListener
+        )
+        locationComponentPlugin.addOnIndicatorBearingChangedListener(
+            onIndicatorBearingChangedListener
+        )
+    }
+
+    private fun onCameraTrackingDismissed() {
+        binding.mapView.apply {
+            location
+                .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+            location
+                .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+            gestures.removeOnMoveListener(onMoveListener)
+        }
     }
 
     override fun onDestroyView() {
+        onCameraTrackingDismissed()
         super.onDestroyView()
         _binding = null
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
